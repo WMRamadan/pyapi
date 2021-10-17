de7d79c9b89590f9caf023838991a782ebd4a1be
Wael Ramadan <thebigguyis@gmail.com>
2021-10-18 01:09:24 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ def read(fname):
 
 
 setup(
-    name="pyapi",
+    name="pyapi-framework",
     version=read("VERSION").strip(),
     author="Wael Ramadan",
     author_email="wamramadan@gmail.com",
     description="A simple python REST api framework.",
     license="GPLv3",
-    packages=["pyapi"],
+    packages=["src"],
     data_files=["VERSION", "requirements.txt", "README.md", "LICENSE"],
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    url="https://github.com/WMRamadan/pyapi",
+    url="https://github.com/WMRamadan/pyapi-framework",
     install_requires=read("requirements.txt").split("\n"),
     include_package_data=True
 )
