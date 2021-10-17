700772edeec7d0734355aba2549d71cf8849cce5
Wael Ramadan <thebigguyis@gmail.com>
2021-10-18 01:32:04 +0300

    update setup.py pypi classifiers

--- setup.py
+++ setup.py
@@ -19,5 +19,16 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/WMRamadan/pyapi-framework",
     install_requires=read("requirements.txt").split("\n"),
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+              'Intended Audience :: Developers',
+              'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+              'Operating System :: MacOS',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 3',
+              'Topic :: Software Development',
+              'Topic :: Software Development :: Libraries :: Application Frameworks',
+          ]
 )
